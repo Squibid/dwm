d776bb0e94a1f02472f66a7db2dccb85d3364134
Squibid <squibid@vrooom.localdomain>
2022-10-01 15:56:36 -0400

    add some stuff and updated README

--- dwm.c
+++ dwm.c
@@ -771,12 +771,12 @@ drawbar(Monitor *m)
 
 	if ((w = m->ww - tw - x) > bh) {
 		if (m->sel) {
-			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
+			drw_setscheme(drw, scheme[SchemeSel]);
 			drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
 			if (m->sel->isfloating)
 				drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
 		} else {
-			drw_setscheme(drw, scheme[SchemeNorm]);
+			drw_setscheme(drw, scheme[SchemeSel]);
 			drw_rect(drw, x, 0, w, bh, 1, 1);
 		}
 	}
@@ -1784,7 +1784,7 @@ tile(Monitor *m)
 	unsigned int i, n, h, mw, my, ty;
 	Client *c;
   
-  if (m->nmaster != 1) /* override layout symbol */
+  if (m->nmaster != nmaster) /* override layout symbol */
 		snprintf(m->ltsymbol, sizeof m->ltsymbol, "[%d]=", m->nmaster);
 	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
 	if (n == 0)
